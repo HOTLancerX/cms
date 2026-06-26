f290cb3b86cb427176c83ab002305d8e4ac2db26
HeRa Khan <heraemt@gmail.com>
2026-06-26 16:07:09 +0600

    1

--- app/admin/[...slug]/page.tsx
+++ app/admin/[...slug]/page.tsx
@@ -34,7 +34,7 @@ export default async function AdminDynamicPage({ params }: AdminPageProps) {
         <main className="cms-page">
             <h1 className="mb-4 block font-bold">{pageDef.label}</h1>
             <>
-                <DynamicComponent />
+                <DynamicComponent params={{ slug }} />
             </>
         </main>
     );
